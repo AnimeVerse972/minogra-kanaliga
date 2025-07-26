cf6f78e68f9f3bfb674e2c0cefec0d1680fa116e
AnimeVerse972 <soibjonovdiyorbek0@gmail.com>
2025-07-26 11:30:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ async def make_subscribe_markup(code):
     keyboard.add(InlineKeyboardButton("✅ Tekshirish", callback_data=f"check_sub:{code}"))
     return keyboard
 
-ADMINS = [6486825926,8017776953]
+ADMINS = [6486825926,7483732504,5959511392]
 
 # === HOLATLAR ===
 class AdminStates(StatesGroup):
